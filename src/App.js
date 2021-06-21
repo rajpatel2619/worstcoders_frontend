fbced5f37d8e8e6e88dd530596e3ccc23db2c8d6
raj patel <kakhilesh79@gmail.com>
2021-06-21 22:48:17 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hey buddy this is me, yathartha
+        <p>hey buddy this is me, yathartha singh
         </p>
         <a
           className="App-link"
