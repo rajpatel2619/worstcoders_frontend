0d939ce39118e2eb148fb7282876468d9f637500
raj patel <kakhilesh79@gmail.com>
2021-06-21 22:44:52 +0530

    after domain

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. this is me.
+        <p>hey buddy this is me, yathartha
         </p>
         <a
           className="App-link"
